214d509a82d4e03d30c1d31dc548ea3fa96da5d9
Jiheng Nie <Niejiheng@gmail.com>
2022-12-05 23:02:38 +0000

    Started creating a sidebar component

---
+++ client/components/sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Sidebar(props) {
+  return (
+    <span className='sidebar-holder'>
+      <ul>
+        <li> Players </li>
+        <li> Schdule </li>
+        <li> History </li>
+        <li> Contact </li>
+      </ul>
+    </span>
+  );
+}
