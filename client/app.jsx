dad9bfff184489471be093642d3c6113218b6ec0
Jiheng Nie <Niejiheng@gmail.com>
2022-12-06 19:47:44 +0000

    Added in parseRoute.js

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import Home from './pages/home';
+import LandingPage from './landing-page';
+import parseRoute from '.lib/parseRoute';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', event => {
+      this.setState({
+        route: parseRoute(window.location.hash)
+      });
+    });
+  }
+
   render() {
-    return <Home />;
+    return <LandingPage />;
   }
 }
