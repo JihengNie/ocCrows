8b6c52290f1df4d319b3f88522373cf2d87eb783
Jiheng Nie <Niejiheng@gmail.com>
2022-12-14 21:16:59 +0000

    Testing player card location

--- client/pages/players-page.jsx
+++ client/pages/players-page.jsx
@@ -11,12 +11,11 @@ export default class PlayersPage extends React.Component {
         <Header />
         <div className='row player-card-container'>
           <PlayerCard number={1} name="Jacob" nickname="Classic" imgSrc='/team/classics.jpg' />
-          <PlayerCard number={0} name="Chris" nickname="Dino" imgSrc='/team/chris.jpg'/>
           <PlayerCard number={2} name="Issac" nickname="OtherName" imgSrc='/team/Issac.jpg' />
+          <PlayerCard number={0} name="Chris" nickname="Dino" imgSrc='/team/chris.jpg'/>
           <PlayerCard number={3} name="Player baz" nickname="YesName" imgSrc='/team/moose.jpg' />
-          <PlayerCard number={4} name="Player etc" nickname="NameName" imgSrc='/team/reno.jpg' />
           <PlayerCard number={0} name="Chris" nickname="Dino" imgSrc='/team/chris.jpg' />
-
+          <PlayerCard number={4} name="Player etc" nickname="NameName" imgSrc='/team/reno.jpg' />
         </div>
         <Footer />
       </div>
